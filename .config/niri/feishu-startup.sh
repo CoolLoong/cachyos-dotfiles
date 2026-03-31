355af9bdb1dbf92588d90f3935ddc065ec42457d
CoolLoong <wingdon@foxmail.com>
2026-03-31 11:21:01 +0800

    feat(config): update system configurations - fcitx5: Add Polish Slavistic phonetic alphabet keyboard layout - mimeapps: Update JetBrains scheme handler desktop entry - niri: Increase Feishu startup delay - noctalia: Update settings to version 59   - Introduce new clipboard, audio, and UI settings   - Add advanced bar and dock customization options   - Implement idle management and performance configurations   - Enhance lock screen and wallpaper settings

--- .config/niri/feishu-startup.sh
+++ .config/niri/feishu-startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 feishu &
-sleep 1
+sleep 2
 niri msg windows | grep -B1 'Title: ""' | grep -v focused | grep 'Window ID' | awk '{print $3}' | tr -d ':' | xargs -I{} niri msg action close-window --id {}
